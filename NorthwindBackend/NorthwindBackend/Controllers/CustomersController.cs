1c91261e29928c515b3c2c9f4f19587cdfdb2d7a
trantien020220 <trantien020220@gmail.com>
2025-11-14 13:55:59 +0700

    Customer API

--- NorthwindBackend/NorthwindBackend/Controllers/CustomersController.cs
+++ NorthwindBackend/NorthwindBackend/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace NorthwindBackend.Controllers
 {
@@ -7,5 +8,33 @@ namespace NorthwindBackend.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        private readonly Models.NorthwindContext _context;
+
+        public CustomersController(Models.NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Models.Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync<Models.Customer>();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Models.Customer>> PostCustomer(Models.Customer customer)
+        {
+            try
+            {
+                _context.Customers.Add(customer);
+                await _context.SaveChangesAsync();
+                return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
     }
 }
