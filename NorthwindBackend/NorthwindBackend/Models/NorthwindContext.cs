8d4aae421608477ed2afdf850e38d64eb8a0bd02
trantien020220 <trantien020220@gmail.com>
2025-11-14 11:29:51 +0700

    add conection string for Northwind database

--- NorthwindBackend/NorthwindBackend/Models/NorthwindContext.cs
+++ NorthwindBackend/NorthwindBackend/Models/NorthwindContext.cs
@@ -1,8 +1,21 @@
-using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace NorthwindBackend.Models;
 
-public class NorthwindContext
+public class NorthwindContext : DbContext
 {
+    public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options)
+    {
+    }
 
+    public DbSet<Customer> Customers { get; set; } = null!;
+    // public DbSet<Order> Orders { get; set; } = null!;
+    // public DbSet<Product> Products { get; set; } = null!;
+    // public DbSet<Category> Categories { get; set; } = null!;
+    // public DbSet<Supplier> Suppliers { get; set; } = null!;
+    // public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+    // public DbSet<Employee> Employees { get; set; } = null!;
+    // public DbSet<Shipper> Shippers { get; set; } = null!;
+    // public DbSet<Region> Regions { get; set; } = null!;
+    // public DbSet<Territory> Territories { get; set; } = null!;
 }
--- NorthwindBackend/NorthwindBackend/Program.cs
+++ NorthwindBackend/NorthwindBackend/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using NorthwindBackend.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 //services
 builder.Services.AddControllers();
+builder.Services.AddDbContext<NorthwindContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindConnection")));
 
 var app = builder.Build();
 
