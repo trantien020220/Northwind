9b746c7a652e9479d009d1d870e04afc7b6500d3
trantien020220 <trantien020220@gmail.com>
2025-11-14 15:01:40 +0700

    add put

--- NorthwindBackend/NorthwindBackend/Controllers/CustomersController.cs
+++ NorthwindBackend/NorthwindBackend/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NorthwindBackend.Models;
 
 namespace NorthwindBackend.Controllers
 {
@@ -7,35 +8,63 @@ namespace NorthwindBackend.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
-        private readonly Models.NorthwindContext _context;
+        private readonly NorthwindContext _context;
 
-        public CustomersController(Models.NorthwindContext context)
+        public CustomersController(NorthwindContext context)
         {
             _context = context;
         }
 
+        //GET: api/customers
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Models.Customer>>> GetCustomers()
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
         {
-            return await _context.Customers.ToListAsync<Models.Customer>();
+            return await _context.Customers.ToListAsync();
         }
 
+        //GET: api/customers/id
         [HttpGet ("{id}")]
-        public async Task<ActionResult<Models.Customer>> GetCustomer(string id)
+        public async Task<ActionResult> GetCustomer(string id)
         {
             var customer = await _context.Customers.FindAsync(id);
             if (customer == null) return NotFound();
             return Ok(customer);
         }
 
+        //POST: api/customers
         [HttpPost]
-        public async Task<ActionResult<Models.Customer>> PostCustomer(Models.Customer customer)
+        public async Task<ActionResult> PostCustomer(Customer customer)
         {
             try
             {
                 _context.Customers.Add(customer);
                 await _context.SaveChangesAsync();
                 return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+            
+        }
+
+        //PUT: api/customers/id
+        [HttpPut ("{id}")]
+        public async Task<ActionResult> UpdateCustomer(string id, [FromBody]Customer customer)
+        {
+            try
+            {
+                if (id != customer.CustomerId)
+                {
+                    return BadRequest("Customer ID mismatch");
+                }
+                if (!await _context.Customers.AnyAsync(c => c.CustomerId == id))
+                {
+                    return NotFound();
+                }
+                _context.Customers.Update(customer);
+                await _context.SaveChangesAsync();
+                return NoContent();
             }
             catch (Exception ex)
             {
