f8b0edf494c911d9c740dd5b4357b0e2fc2ed9e3
trantien020220 <trantien020220@gmail.com>
2025-11-18 17:10:04 +0700

    add Repository service

--- NorthwindBackend/NorthwindBackend/Program.cs
+++ NorthwindBackend/NorthwindBackend/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NorthwindBackend.Models;
 using NorthwindBackend.Services;
+using NorthwindBackend.Repositories;
 
 var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindConnection")));
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 var app = builder.Build();
 
 app.UseCors(MyAllowSpecificOrigins);
