f24cfd3551f475d2b7ecd59dcf81e5433a283b0b
trantien020220 <trantien020220@gmail.com>
2025-11-18 17:17:34 +0700

    Repository

--- NorthwindBackend/NorthwindBackend/Repositories/IGenericRepository.cs
+++ NorthwindBackend/NorthwindBackend/Repositories/IGenericRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace NorthwindBackend.Repositories;
 
-public interface IGenericRepository
+public interface IGenericRepository<T> where T : class
 {
-    
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(object id);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task SaveAsync();
 }
--- NorthwindBackend/NorthwindBackend/Services/CustomerService.cs
+++ NorthwindBackend/NorthwindBackend/Services/CustomerService.cs
@@ -2,58 +2,60 @@ using Microsoft.EntityFrameworkCore;
 using NorthwindBackend.DTOs;
 using NorthwindBackend.Models;
 using AutoMapper;
+using NorthwindBackend.Repositories;
 
 namespace NorthwindBackend.Services;
 
 public class CustomerService : ICustomerService
 {
-    private readonly IMapper _mapper;        
-    private readonly NorthwindContext _context;
+    private readonly IMapper _mapper;
+    private readonly IGenericRepository<Customer> _customerRepository;
 
-    public CustomerService(NorthwindContext context, IMapper mapper)
+    public CustomerService(IGenericRepository<Customer> customerRepository, IMapper mapper)
     {
-        _context = context;
+        _customerRepository = customerRepository;
         _mapper = mapper;
     }
 
     public async Task<IEnumerable<CustomerDto>> GetCustomers()
     {
-        var customers = await _context.Customers.ToListAsync();
+        var customers = await _customerRepository.GetAllAsync();
         return _mapper.Map<IEnumerable<CustomerDto>>(customers);
     }
 
     public async Task<CustomerDto?> GetCustomerbyId(string id)
     {
-        var customer = await _context.Customers.FindAsync(id);
+        var customer = await _customerRepository.GetByIdAsync(id);
         return _mapper.Map<CustomerDto>(customer);
     }
 
     public async Task<CustomerDto> PostCustomer(CreateCustomerDto dto)
     {
         var customer = _mapper.Map<Customer>(dto);  
-        _context.Customers.Add(customer);
-        await _context.SaveChangesAsync();
+        _customerRepository.AddAsync(customer);
+        await _customerRepository.SaveAsync();
         return _mapper.Map<CustomerDto>(customer);
     }
 
     public async Task<bool> PutCustomer(string id, CreateCustomerDto dto)
     {
-        var customer = await _context.Customers.FindAsync(id);
+        var customer = await _customerRepository.GetByIdAsync(id);
         if (customer == null) return false;
 
         _mapper.Map(dto, customer);
-
-        await _context.SaveChangesAsync();
+        
+        _customerRepository.Update(customer);
+        await _customerRepository.SaveAsync();
         return true;
     }
 
     public async Task<bool> DeleteCustomer(string id)
     {
-        var customer = await _context.Customers.FindAsync(id);
+        var customer = await _customerRepository.GetByIdAsync(id);
         if (customer == null) return false;
 
-        _context.Customers.Remove(customer);
-        await _context.SaveChangesAsync();
+        _customerRepository.Delete(customer);
+        await _customerRepository.SaveAsync();
         return true;
     }
 }
