2e321ef0923812c3a31d0d18f5e63dfefad6d790
trantien020220 <trantien020220@gmail.com>
2025-11-19 17:15:20 +0700

    responses

---
+++ NorthwindBackend/NorthwindBackend/Profiles/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace NorthwindBackend.Profiles;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+
+    public ApiResponse() { }
+
+    public ApiResponse(bool success, string message, T? data = default)
+    {
+        Success = success;
+        Message = message;
+        Data = data;
+    }
+    
+    public static ApiResponse<T> Ok(T data, string message = "Success")
+    {
+        return new ApiResponse<T>(true, message, data);
+    }
+    
+    public static ApiResponse<T> Fail(string message)
+    {
+        return new ApiResponse<T>(false, message, default);
+    }
+}
