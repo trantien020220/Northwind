d9c739633115e77a2978c8ec5fc1cec836e0cceb
trantien020220 <trantien020220@gmail.com>
2025-11-19 17:23:00 +0700

    no commit

--- NorthwindBackend/NorthwindBackend/Models/Responses/PagedResponse.cs
+++ NorthwindBackend/NorthwindBackend/Models/Responses/PagedResponse.cs
@@ -1,22 +1,21 @@
 ﻿using NorthwindBackend.Profiles;
 
-namespace NorthwindBackend.Models.Responses
-{
-    public class PagedResponse<T> : ApiResponse<T>
-    {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-        public int TotalRecords { get; set; }
-        public int TotalPages { get; set; }
+namespace NorthwindBackend.Models.Responses;
 
-        public PagedResponse(
-            T data, int pageNumber, int pageSize, int totalRecords, string message = "Success"
-        ) : base(true, message, data)
-        {
-            PageNumber = pageNumber;
-            PageSize = pageSize;
-            TotalRecords = totalRecords;
-            TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
-        }
+public class PagedResponse<T> : ApiResponse<T>
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalRecords { get; set; }
+    public int TotalPages { get; set; }
+
+    public PagedResponse(
+        T data, int pageNumber, int pageSize, int totalRecords, string message = "Success"
+    ) : base(true, message, data)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalRecords = totalRecords;
+        TotalPages = (int)Math.Ceiling(totalRecords / (double)pageSize);
     }
 }
