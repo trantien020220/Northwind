2ce628919f2e75845aa3f54f5fa48f86baa30ade
trantien020220 <trantien020220@gmail.com>
2025-11-21 12:00:18 +0700

    order service

---
+++ NorthwindBackend/NorthwindBackend/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using NorthwindBackend.DTOs;
+
+namespace NorthwindBackend.Services;
+public interface IOrderService
+{
+    Task<IEnumerable<OrderDto>> GetOrders();
+    Task<OrderDto?> GetOrderById(int id);
+    Task<OrderDto> CreateOrder(CreateOrderDto dto);
+    Task<bool> UpdateOrder(int id, CreateOrderDto dto);
+    Task<bool> DeleteOrder(int id);
+}
---
+++ NorthwindBackend/NorthwindBackend/Services/OrderService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBackend.DTOs;
+using NorthwindBackend.Models;
+using AutoMapper;
+using NorthwindBackend.UnitOfWork;
+
+namespace NorthwindBackend.Services;
+
+
+public class OrderService : IOrderService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public OrderService(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<OrderDto>> GetOrders()
+    {
+        var orders = await _unitOfWork.Orders.GetAllAsync();
+        return _mapper.Map<IEnumerable<OrderDto>>(orders);
+    }
+
+    public async Task<OrderDto?> GetOrderById(int id)
+    {
+        var order = await _unitOfWork.Orders.GetByIdAsync(id);
+        return _mapper.Map<OrderDto>(order);
+    }
+
+    public async Task<OrderDto> CreateOrder(CreateOrderDto dto)
+    {
+        var order = _mapper.Map<Order>(dto);
+        await _unitOfWork.Orders.AddAsync(order);
+        await _unitOfWork.Orders.SaveAsync();
+        return _mapper.Map<OrderDto>(order);
+    }
+
+    public async Task<bool> UpdateOrder(int id, CreateOrderDto dto)
+    {
+        var order = await _unitOfWork.Orders.GetByIdAsync(id);
+        if (order == null) return false;
+
+        _mapper.Map(dto, order);
+        _unitOfWork.Orders.Update(order);
+        await _unitOfWork.Orders.SaveAsync();
+        return true;
+    }
+
+    public async Task<bool> DeleteOrder(int id)
+    {
+        var order = await _unitOfWork.Orders.GetByIdAsync(id);
+        if (order == null) return false;
+
+        _unitOfWork.Orders.Delete(order);
+        await _unitOfWork.Orders.SaveAsync();
+        return true;
+    }
+}
