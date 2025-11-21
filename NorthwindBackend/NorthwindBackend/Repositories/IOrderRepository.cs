bf7756c5e4b6a205165e44b40335a03370aebf7a
trantien020220 <trantien020220@gmail.com>
2025-11-21 12:01:38 +0700

    order repository

---
+++ NorthwindBackend/NorthwindBackend/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using NorthwindBackend.Models;
+
+namespace NorthwindBackend.Repositories;
+
+public interface IOrderRepository : IGenericRepository<Order>
+{
+    Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(string customerId);
+    // Task<IEnumerable<Order>> GetOrdersByEmployeeIdAsync(int employeeId);
+}
---
+++ NorthwindBackend/NorthwindBackend/Repositories/OrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBackend.Models;
+
+namespace NorthwindBackend.Repositories;
+
+public class OrderRepository : GenericRepository<Order>, IOrderRepository
+{
+    private readonly NorthwindContext _context;
+    public OrderRepository(NorthwindContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(string customerId)
+    {
+        return await _context.Orders.Where(o => o.CustomerId == customerId).ToListAsync();
+    }
+
+    // public async Task<IEnumerable<Order>> GetOrdersByEmployeeIdAsync(int employeeId)
+    // {
+    //     return await _dbSet.Where(o => o.EmployeeId == employeeId).ToListAsync();
+    // }
+}
