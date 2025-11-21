e7e20cece05c4061532d357e4ca69dd9ae06b049
trantien020220 <trantien020220@gmail.com>
2025-11-21 12:00:43 +0700

    order

---
+++ NorthwindBackend/NorthwindBackend/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace NorthwindBackend.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; } = null!;
+        public DateTime OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public decimal? Freight { get; set; }
+        public string? ShipName { get; set; }
+        public string? ShipCity { get; set; }
+        public string? ShipCountry { get; set; }
+    }
+}
