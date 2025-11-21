ae987718bb03eda6fdc78840fcab9e73f81236d2
trantien020220 <trantien020220@gmail.com>
2025-11-21 11:59:17 +0700

    add order validation

---
+++ NorthwindBackend/NorthwindBackend/Validators/OrderValidatior.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using NorthwindBackend.DTOs;
+
+public class OrderValidatior : AbstractValidator<CreateOrderDto>
+{
+    public  OrderValidatior()
+    {
+        RuleFor(x => x.OrderId)
+                .NotEmpty().WithMessage("ID is required")
+                .MaximumLength(5).WithMessage("ID must have 5 characters")
+                .MinimumLength(5).WithMessage("ID must have 5 characters");
+    }
+}
