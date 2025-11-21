a457093687f2dd2ed2772ec5163c6ae53c3f756b
trantien020220 <trantien020220@gmail.com>
2025-11-21 13:59:40 +0700

    add rule for orderdate and ship country

--- NorthwindBackend/NorthwindBackend/Validators/OrderValidatior.cs
+++ NorthwindBackend/NorthwindBackend/Validators/OrderValidatior.cs
@@ -5,9 +5,16 @@ public class OrderValidatior : AbstractValidator<CreateOrderDto>
 {
     public  OrderValidatior()
     {
-        RuleFor(x => x.OrderId)
-                .NotEmpty().WithMessage("ID is required")
+        RuleFor(x => x.CustomerId)
+                .NotEmpty().WithMessage("CustomerID is required")
                 .MaximumLength(5).WithMessage("ID must have 5 characters")
                 .MinimumLength(5).WithMessage("ID must have 5 characters");
+
+        RuleFor(order => order.OrderDate)
+            .NotEmpty().WithMessage("Order Date is required");
+
+        RuleFor(order => order.ShipCountry)
+            .NotEmpty().WithMessage("ShipCountry is required")
+            .MaximumLength(50).WithMessage("Country not above 50 characters");
     }
 }
