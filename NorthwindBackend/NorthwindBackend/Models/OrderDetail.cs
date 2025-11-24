f264e8fae423cfdb406681106b2f788cf58cf482
trantien020220 <trantien020220@gmail.com>
2025-11-24 13:49:56 +0700

    add orderdetail model

---
+++ NorthwindBackend/NorthwindBackend/Models/OrderDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace NorthwindBackend.Models;
+
+public class OrderDetail
+{
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public short Quantity { get; set; }
+    public float Discount { get; set; }
+    
+    public Order Order { get; set; } = null!;
+    public Product Product { get; set; } = null!;
+}
