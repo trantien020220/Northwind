0f386c0837c708dfe61e32e46c7069dfbacaa1eb
trantien020220 <trantien020220@gmail.com>
2025-11-27 09:32:45 +0700

    update

--- NorthwindBackend/NorthwindBackend/DTOs/Order/OrderDto.cs
+++ NorthwindBackend/NorthwindBackend/DTOs/Order/OrderDto.cs
@@ -6,14 +6,8 @@ public class OrderDto
     public string CustomerId { get; set; } = null!;
     public int? EmployeeId { get; set; }
     public DateTime? OrderDate { get; set; }
-    // public DateTime? RequiredDate { get; set; }
-    // public DateTime? ShippedDate { get; set; }
-    // public int? ShipVia { get; set; }
-    // public decimal? Freight { get; set; }
     public string? ShipName { get; set; }
     public string? ShipAddress { get; set; }
-    // public string? ShipCity { get; set; }
-    // public string? ShipRegion { get; set; }
     public string? ShipPostalCode { get; set; }
     public string? ShipCountry { get; set; }
 }
