ffd0f98d4308b65798b0cc61f39f39c77d18ffd2
trantien020220 <trantien020220@gmail.com>
2025-11-27 09:33:04 +0700

    update

--- NorthwindBackend/NorthwindBackend/Models/Order.cs
+++ NorthwindBackend/NorthwindBackend/Models/Order.cs
@@ -3,11 +3,11 @@ public class Order
 {
     public int OrderId { get; set; }
     public string CustomerId { get; set; } = null!;
-    public int EmployeeId { get; set; }
-    public DateTime OrderDate { get; set; }
+    public int? EmployeeId { get; set; }
+    public DateTime? OrderDate { get; set; }
     public DateTime? RequiredDate { get; set; }
     public DateTime? ShippedDate { get; set; }
-    public int ShipVia { get; set; }
+    public int? ShipVia { get; set; }
     public decimal? Freight { get; set; }
     public string? ShipName { get; set; }
     public string? ShipAddress { get; set; }
--- NorthwindBackend/NorthwindBackend/Models/OrderDetail.cs
+++ NorthwindBackend/NorthwindBackend/Models/OrderDetail.cs
@@ -3,10 +3,11 @@
 public class OrderDetail
 {
     public int OrderId { get; set; }
-    public int ProductId { get; set; }
-    public decimal UnitPrice { get; set; }
-    public short Quantity { get; set; }
-    public float Discount { get; set; }
+    public int? ProductId { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public short? Quantity { get; set; }
+    public decimal? Discount { get; set; }
+
     
     public Order Order { get; set; } = null!;
     public Product Product { get; set; } = null!;
