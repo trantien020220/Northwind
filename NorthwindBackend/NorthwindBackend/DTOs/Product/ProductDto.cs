c70b08f60e737a86644f8b6dae4b606d920365d8
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:07:22 +0700

    update

--- NorthwindBackend/NorthwindBackend/DTOs/Product/ProductDto.cs
+++ NorthwindBackend/NorthwindBackend/DTOs/Product/ProductDto.cs
@@ -7,5 +7,4 @@ public class ProductDto
     public int? SupplierId { get; set; }
     public int? CategoryId { get; set; }
     public decimal? UnitPrice { get; set; }
-    // public short? UnitsInStock { get; set; }
 }
