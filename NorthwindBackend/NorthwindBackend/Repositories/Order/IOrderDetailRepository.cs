5dce629d17f42b5a65bdb49ea2819c1e5e4b27bb
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:24:27 +0700

    order detail repository

---
+++ NorthwindBackend/NorthwindBackend/Repositories/Order/IOrderDetailRepository.cs
@@ -0,0 +1,9 @@
+﻿using NorthwindBackend.Models;
+
+namespace NorthwindBackend.Repositories;
+
+public interface IOrderDetailRepository : IGenericRepository<OrderDetail>
+{
+    Task<OrderDetail?> GetByIdAsync(int orderId, int productId);
+    Task<IEnumerable<OrderDetail>> GetOrderDetailsByOrderIdAsync(int orderId);
+}
---
+++ NorthwindBackend/NorthwindBackend/Repositories/Order/OrderDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBackend.Models;
+using NorthwindBackend.Repositories;
+
+public class OrderDetailRepository : GenericRepository<OrderDetail>, IOrderDetailRepository
+{
+    private readonly NorthwindContext _context;
+
+    public OrderDetailRepository(NorthwindContext context) : base(context)
+    {
+        _context = context;
+    }
+    
+    public async Task<OrderDetail?> GetByIdAsync(int orderId, int productId)
+    {
+        return await _context.OrderDetail
+            .FirstOrDefaultAsync(od => od.OrderId == orderId && od.ProductId == productId);
+    }
+
+    public async Task<IEnumerable<OrderDetail>> GetOrderDetailsByOrderIdAsync(int orderId)
+    {
+        return await _context.OrderDetail
+            .Where(od => od.OrderId == orderId)
+            .ToListAsync();
+    }
+}
