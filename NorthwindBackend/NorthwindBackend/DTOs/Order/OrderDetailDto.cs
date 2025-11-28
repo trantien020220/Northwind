b72793046de13434fed6c6723ca5cfc40fc4759b
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:24:51 +0700

    order detail dto

---
+++ NorthwindBackend/NorthwindBackend/DTOs/Order/OrderDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBackend.DTOs;
+
+public class OrderDetailDto
+{
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public short Quantity { get; set; }
+    public float Discount { get; set; }
+}
