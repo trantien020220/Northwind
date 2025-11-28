fc2d4716f1e0634119227e5870adfc15e548dbfd
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:09:10 +0700

    suppiler validation

--- NorthwindBackend/NorthwindBackend/Validators/SupplierValidator.cs
+++ NorthwindBackend/NorthwindBackend/Validators/SupplierValidator.cs
@@ -1,6 +1,21 @@
-﻿namespace NorthwindBackend.Validators;
+﻿using FluentValidation;
+using NorthwindBackend.DTOs;
 
-public class SupplierValidator
+namespace NorthwindBackend.Validators;
+
+public class SupplierValidator : AbstractValidator<CreateSupplierDto>
 {
-    
+    public SupplierValidator()
+    {
+        RuleFor(supplier => supplier.CompanyName)
+            .NotEmpty().WithMessage("Company name is required")
+            .MaximumLength(50).WithMessage("Company name cant have above 50 characters");
+
+        RuleFor(supplier => supplier.Phone)
+            .NotEmpty().WithMessage("Phone number is required")
+            .Matches(@"^\d{9,11}$").WithMessage("Phone must be from 9 to 11 digits");
+
+        RuleFor(supplier => supplier.Country)
+            .NotEmpty().WithMessage("Country is required");    }
 }
+
