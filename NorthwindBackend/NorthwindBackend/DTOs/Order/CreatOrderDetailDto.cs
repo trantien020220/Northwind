89b50cff9e01f05bc028080327e5c1d61d3f2f6b
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:13:01 +0700

    add new create order detail dto

---
+++ NorthwindBackend/NorthwindBackend/DTOs/Order/CreatOrderDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthwindBackend.DTOs;
+
+public class CreateOrderDetailDto
+{
+    public int OrderId { get; set; }
+    public int ProductId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public short Quantity { get; set; }
+    public float Discount { get; set; }
+}
