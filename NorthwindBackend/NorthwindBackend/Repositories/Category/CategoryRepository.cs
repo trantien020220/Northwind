2dc847a93eeab61076ebc2da8a5b89d03cc62c11
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:10:37 +0700

    add category repository

---
+++ NorthwindBackend/NorthwindBackend/Repositories/Category/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindBackend.Models;
+
+namespace NorthwindBackend.Repositories;
+
+public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+{
+    private readonly NorthwindContext _context;
+
+    public CategoryRepository(NorthwindContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public new IQueryable<Category> GetAllQueryable()
+    {
+        return _context.Categories.AsQueryable();
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoryFilteredAsync(
+        int? categoryId,
+        string? categoryName,
+        string? description,
+        string? sortBy,
+        bool ascending = true)
+    {
+        var query = _context.Categories.AsQueryable();
+
+        if (categoryId.HasValue)
+            query = query.Where(p => p.CategoryId == categoryId.Value);
+
+        if (!string.IsNullOrEmpty(categoryName))
+            query = query.Where(p => p.CategoryName.Contains(categoryName));
+
+        if (!string.IsNullOrEmpty(description))
+            query = query.Where(p => p.Description != null && p.Description.Contains(description));
+        
+
+        if (!string.IsNullOrEmpty(sortBy))
+        {
+            query = sortBy.ToLower() switch
+            {
+                "categoryname" => ascending ? query.OrderBy(p => p.CategoryName) : query.OrderByDescending(p => p.CategoryName),
+                _ => query
+            };
+        }
+
+        return await query.ToListAsync();
+    }
+}
---
+++ NorthwindBackend/NorthwindBackend/Repositories/Category/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using NorthwindBackend.Models;
+
+namespace NorthwindBackend.Repositories;
+
+public interface ICategoryRepository : IGenericRepository<Category>
+{
+    new IQueryable<Category> GetAllQueryable();
+
+    Task<IEnumerable<Category>> GetCategoryFilteredAsync(
+        int? categoryId,
+        string? categoryName,
+        string? description,
+        string? sortBy,
+        bool ascending = true);
+}
