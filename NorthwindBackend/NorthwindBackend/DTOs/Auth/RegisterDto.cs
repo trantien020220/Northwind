b767ec31029b96544aded2926b7413f8771da08e
trantien020220 <trantien020220@gmail.com>
2025-11-28 16:12:30 +0700

    register dto

---
+++ NorthwindBackend/NorthwindBackend/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NorthwindBackend.DTOs.Auth;
+
+public class RegisterDto
+{
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
