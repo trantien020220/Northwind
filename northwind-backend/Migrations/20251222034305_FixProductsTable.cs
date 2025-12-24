a16672d7c535d034b67d5bb09033e3503cea86a8
trantien020220 <trantien020220@gmail.com>
2025-12-24 08:56:42 +0700

    Remove file from git tracking

---
+++ northwind-backend/Migrations/20251222034305_FixProductsTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthwindBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProductsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Discontinued",
+                table: "Products",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantityPerUnit",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Discontinued",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
---
+++ northwind-backend/Migrations/20251222040438_AddIsSuperAdminToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthwindBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsSuperAdminToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSuperAdmin",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSuperAdmin",
+                table: "AspNetUsers");
+        }
+    }
+}
